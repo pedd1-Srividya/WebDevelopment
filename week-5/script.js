3dde324ed239e582d6583aef53dd91326491320a
Peddi Srividya <srividyapeddi04@gmail.com>
2026-01-16 20:00:19 +0530

    Add files via upload

---
+++ week-5/script.js
@@ -0,0 +1,8 @@
+// Products page cart array
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+function addToCart(name, price){
+  cart.push({name, price});
+  localStorage.setItem("cart", JSON.stringify(cart));
+  alert(name + " added to cart!");
+}
